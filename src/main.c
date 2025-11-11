86968499a25035d645a957e2a8963bb79c2aa0ba
SplatDem <splatdem194@gmail.com>
2025-11-11 23:15:42 +0300

    ADD: Repl (but without saving the result between lines, because this is a compiler); TODO: Update README.md

--- src/main.c
+++ src/main.c
@@ -1,19 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "builtin_ir.h"
 
 #define DEFAULT_CODE_BUF_SIZE 1024
 
 void compile (FILE* output_file, char *token);
+void imm_mode ();
 
 int
 main (int argc, char **argv)
 {
-  if (argc < 3)
+  if (argc < 2)
   {
-    printf ("Usage: %s input.li output.asm\n", argv[0]);
+    printf ("Usage: %s input.li output.asm\n"
+            "Or %s --imm-mode | -i\n", argv[0], argv[0]);
     exit (EXIT_FAILURE);
+  }
+
+  if (!strcmp(argv[1], "--imm-mode")
+       || !strcmp(argv[1], "-i"))
+  {
+    imm_mode ();
   }
   
   FILE *fd = fopen(argv[1], "r");
@@ -31,7 +40,6 @@ main (int argc, char **argv)
   }
 
   char *code_buf = (char*)malloc (DEFAULT_CODE_BUF_SIZE);
-
 
   fputs ("format ELF64 executable\n"
          "entry _start\n"
@@ -280,4 +288,127 @@ compile (FILE* output_file, char *token)
       break;
     }
   }
+}
+
+void
+imm_mode ()
+{
+  puts ("\033[33mWARNING\033[0m:\nThis is not an interpreter.\n"
+        "All your input compiles to native assembly\n"
+        "in fly and writes to the ./lilang_temp_output_imm_mode.asm"
+        "and lilang_temp_output_imm_mode_output.\n"
+        "1: ASCII text, 2: binary file.\n"
+        "To exit properly with this file removed, you must type\n"
+        "':exit'\n");
+
+  char *code_buf = (char *)malloc (DEFAULT_CODE_BUF_SIZE);
+  if (code_buf == NULL)
+  {
+    puts ("Failed to allocate memory");
+    exit (EXIT_FAILURE);
+  }
+
+  bool running = true;
+  while (running)
+  {
+    printf ("> ");
+    if (fgets (code_buf, DEFAULT_CODE_BUF_SIZE, stdin) != NULL)
+    {
+        size_t len = strlen (code_buf);
+        if (len > 0 && code_buf[len - 1] == '\n')
+        {
+            code_buf[len - 1] = '\0';
+        }
+
+        if (strcmp (code_buf, ":exit") == 0)
+        {
+          puts ("Well done! Deleting garbage");
+          running = false;
+          continue;
+        }
+
+        char *comment_pos = strchr(code_buf, ';');
+        if (comment_pos != NULL)
+        {
+            *comment_pos = '\0';
+            len = strlen(code_buf);
+            if (len == 0)
+            {
+                continue;
+            }
+        }
+
+        FILE *output_file = fopen ("./lilang_temp_output_imm_mode.asm", "w");
+        if (output_file == NULL)
+        {
+          puts ("Failed to open temporary output file");
+          continue;
+        }
+    
+        fputs ("format ELF64 executable\n"
+               "entry _start\n"
+               "exit:\n"
+               "\tmov rax, 60\n"
+               "\tmov rdi, 0\n"
+               "\tsyscall\n"
+               "dump:\n"
+               "     mov r9, -3689348814741910323\n"
+               "     sub rsp, 40\n"
+               "     mov BYTE [rsp+31], 10\n"
+               "     lea rcx, [rsp+30]\n"
+               "dump_L2:\n"
+               "     mov rax, rdi\n"
+               "     lea r8, [rsp+32]\n"
+               "     mul r9\n"
+               "     mov rax, rdi\n"
+               "     sub r8, rcx\n"
+               "     shr rdx, 3\n"
+               "     lea rsi, [rdx+rdx*4]\n"
+               "     add rsi, rsi\n"
+               "     sub rax, rsi\n"
+               "     add eax, 48\n"
+               "     mov BYTE [rcx], al\n"
+               "     mov rax, rdi\n"
+               "     mov rdi, rdx\n"
+               "     mov rdx, rcx\n"
+               "     sub rcx, 1\n"
+               "     cmp rax, 9\n"
+               "     ja dump_L2\n"
+               "     lea rax, [rsp+32]\n"
+               "     mov edi, 1\n"
+               "     sub rdx, rax\n"
+               "     xor eax, eax\n"
+               "     lea rsi, [rsp+32+rdx]\n"
+               "     mov rdx, r8\n"
+               "     mov rax, 1\n"
+               "     syscall\n"
+               "     add rsp, 40\n"
+               "      ret\n"
+               "_start:\n", output_file);
+
+        char *token = strtok (code_buf, " ");
+        while (token != NULL)
+        {
+          compile (output_file, token);
+          token = strtok (NULL, " ");
+        }
+
+        fputs ("\tcall exit\n", output_file);
+        fclose (output_file);
+
+        system ("fasm lilang_temp_output_imm_mode.asm > /dev/null 2>&1");
+        system ("chmod +x lilang_temp_output_imm_mode");
+
+        system ("./lilang_temp_output_imm_mode");
+    }
+    else 
+    {
+        puts ("WRONG!\n");
+    }
+  }
+
+  free (code_buf);
+  remove ("./lilang_temp_output_imm_mode.asm");
+  remove ("./lilang_temp_output_imm_mode");
+  exit (EXIT_SUCCESS);
 }
